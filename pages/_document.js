39fba7ab066ff767f3677d04b92ae8ce666ff8fd
joy <joy.1993@hotmail.com>
2020-04-30 22:15:04 +0600

    Meta Added

--- pages/_document.js
+++ pages/_document.js
@@ -13,8 +13,9 @@ class MyDocument extends Document {
             <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <title>Joyprokash - Backend || Laravel || NextJS || React Developer</title>
-            <meta name="description" content="" />
-            <meta name="author" content="" />
+            <meta name="description" content="Five plus years of experience in Backend Development  Experienced with all stages of the development cycle for Software projects. Well-versed in numerous programming languages including JavaScript, SQL, JQuery and PHP, ReactJs. Strong background in project management and SDLC. Sound Knowledge on Famous PHP & JavaScript Frameworks (Laravel, Yii2, Cake PHP, NextJs) ." />
+            <meta name="keywords" content="HTML,CSS,XML,JavaScript,php,web development,web developer,software, software developer,Link3 Technologies,link3,joy,joyprokash,joyprokash chakraborty,Angularjs,nodejs" />
+            <meta name="author" content="Joyprokash Chakrabarty" />
             <link rel="stylesheet" type="text/css"  href="css/bootstrap.css" />
             <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css" />
             <link rel="stylesheet" type="text/css"  href="css/style.css" />

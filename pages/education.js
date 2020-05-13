201de55cfecf6c4a541d958eaedf64c51f2f554a
joy <joy.1993@hotmail.com>
2020-05-14 00:06:29 +0600

    Education Added

--- pages/education.js
+++ pages/education.js
@@ -34,18 +34,6 @@ export default function Education() {
                         </div>
                       </div>
                     </li>
-                    <li>
-                      <div className="timeline-image"><h4>2004 <br /> - <br /> 2007 </h4></div>
-                      <div className="timeline-panel">
-                        <div className="timeline-heading">
-                          <h4>School Name</h4>
-                          <h4 className="subheading">High School Degree</h4>
-                        </div>
-                        <div className="timeline-body">
-                          <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diamcommodo nibh ante facilisis.</p>
-                        </div>
-                      </div>
-                    </li>
                   </ul>
               </div>
           </div>
